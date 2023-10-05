e0fe9b444095518e427b202603c4751b54cc54e8
KwakDain <dyne828@naver.com>
2023-10-05 23:43:20 +0900

    Add File [hw2.c]

---
+++ hw3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int i, j, space, rows = 5;
+
+    for (i = 1; i <= rows; i++) {
+        for (space = 1; space <= rows - i; space++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
